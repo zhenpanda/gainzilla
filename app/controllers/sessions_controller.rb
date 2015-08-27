1eb4cfe3403bf93f43b6340c3bc8b0a138850621
zhenting zhou <zhentzhou@gmail.com>
2015-08-27 09:21:56 -0400

    working login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,18 @@
 class SessionsController < ApplicationController
 
-	def new
-	end
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      redirect_to welcome_index_path
+    else
+      redirect_to root_path
+    end
+  end
 
-	def create
-	  user = User.authenticate(params[:name], params[:password])
-	  if user
-	    session[:user_id] = user.id
-	    redirect_to root_url, :notice => "Logged in!"
-	  else
-	    flash.now.alert = "Invalid name or password"
-	    render "new"
-	  end
-	end
-
-	def destroy
-	  session[:user_id] = nil
-	  redirect_to root_url, :notice => "Logged out!"
-	end
+  def destroy
+    session[:current_user_id] = nil
+    redirect_to root_path
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,39 @@
 class UsersController < ApplicationController
 
-	  def create
-	    @user = User.new(user_params)
-	  if @user.save
-	    redirect_to root_url, :notice => "Signed up!"
-	  else
-	    render "new"
-	  end
+  before_action :authenticate, except: [:new, :create]
 
-	  private
+  def new
+    @user = User.new
+  end
 
-	  def user_params
-	    params.require(:user).permit(:name, :password, :salt, :encrypted_password)
-	  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:current_user_id] = @user.id
+      redirect_to welcome_index_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  def destroy
+    @user = User.find(params[:id])
+    session[:current_user_id] = nil
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+
 end
