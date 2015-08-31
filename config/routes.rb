50d751b4db3267ebf17273ac1690097f8edcd381
zhenting zhou <zhentzhou@gmail.com>
2015-08-31 11:07:24 -0400

    test assets rake

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  root to: 'users#new'
+  root :to => 'users#new'
   
   resources :users, only: [:new, :create, :show, :destroy]
-  resources :welcome
   resources :weights, only: [:new, :create]
+  resources :welcome
 
   get 'sessions/new' => 'sessions#new'
   post 'sessions' => 'sessions#create'
